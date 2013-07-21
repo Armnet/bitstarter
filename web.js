df562434011a0d11213559de8b87a6cc18b379d3
Mariusz <jedrzejczakwebtools@gmail.com>
2013-07-21 08:55:54 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
     var indeks = fs.readFileSync("index.html");
-    response.send(indeks.toString('UTF-8'));
+    response.send(indeks.toString("UTF-8");
 });
 
 var port = process.env.PORT || 5000;
